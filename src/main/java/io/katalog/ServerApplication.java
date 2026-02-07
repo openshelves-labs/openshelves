535bb66928c10e5886c928479a7734a090f7b7bf
Arpan Mahanty <arpan.mahanty.007@gmail.com>
2026-02-08 01:35:54 +0530

    Add initial application configuration and server setup

---
+++ src/main/java/io/katalog/ServerApplication.java
@@ -0,0 +1,14 @@
+package io.katalog;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class ServerApplication {
+
+    static void main(String... args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+}

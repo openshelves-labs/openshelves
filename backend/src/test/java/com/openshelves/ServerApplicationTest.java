e029b1c8955b9125d9ad802aae866dbb36e82844
Arpan Mahanty <arpan.mahanty.007@gmail.com>
2026-07-26 13:48:35 +0530

    test: disable ServerApplicationTest to prevent execution during test runs

--- backend/src/test/java/com/openshelves/ServerApplicationTest.java
+++ backend/src/test/java/com/openshelves/ServerApplicationTest.java
@@ -1,8 +1,10 @@
 package com.openshelves;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Disabled
 @SpringBootTest
 class ServerApplicationTest {
 
